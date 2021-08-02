387172243ea46c522ea03f08c535fb6488f517c2
Boris Demeshev <boris.demeshev@gmail.com>
2021-08-02 11:46:45 +0300

    encoding

--- R/date_to_string.R
+++ R/date_to_string.R
@@ -6,12 +6,15 @@
 #'
 #' @param date date in the following format: "2019-02-23"
 #' @param freq equal to 4 for quarters, equal to 12 for months
+#' @param encoding encoding, 'UTF-8' by default, try 'windows-1251' on windows :)
 #' @return date in the form of the string
 #' @export
 #' @examples
 #' date <- c("2019-02-23") # date <- c("2019-02-23", "2011-01-07")
 #' date_to_string(date, freq = 12)
-date_to_string <- function(date, freq = 12) {
+date_to_string <- function(date, freq = 12, encoding = 'UTF-8') {
+  encoding = match.arg(encoding)
+
   date_splited <- stringr::str_split(date, "-", simplify = TRUE)
   if (freq == 12) {
     date_month <- dplyr::case_when(
@@ -30,7 +33,7 @@ date_to_string <- function(date, freq = 12) {
     )
     #date_string <- stringr::str_c(date_month, date_splited[,1], sep = " ")
     date_string <- paste(date_month, date_splited[,1])
-    
+
     }
   if (freq == 4) {
     date_quarter <- dplyr::case_when(
@@ -40,6 +43,9 @@ date_to_string <- function(date, freq = 12) {
       date_splited[,2] == "10" ~ "IV квартал"
     )
     date_string <- paste(date_quarter, date_splited[,1])
+  }
+  if (!encoding == 'UTF-8') {
+    date_string = stringi::stri_conv(date_string, from = 'UTF-8', to = encoding)
   }
   return(date_string)
 }
